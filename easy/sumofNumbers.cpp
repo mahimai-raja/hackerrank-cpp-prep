3fc1ada1d62df700db95b6b74cb7584abb21f176
Mahimai Raja J <mahimairaja3@gmail.com>
2022-11-24 21:22:47 +0530

    Adding up numbers

---
+++ easy/sumofNumbers.cpp
@@ -0,0 +1,22 @@
+/*
+
+Task :
+Read  numbers from stdin and print their sum to stdout.
+
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a+b+c;
+    
+    return 0;
+}
